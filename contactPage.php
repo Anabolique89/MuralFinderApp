a7c3647af5d6771c96f41a93d2d58977a8e9929f
Nina Popovici <adrenocrom_pn@yahoo.com>
2024-01-05 14:25:25 +0000

    separated some css and removed footer for now

--- contactPage.php
+++ contactPage.php
@@ -10,7 +10,7 @@ include_once "header.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="style.css"> -->
-    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="contact.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>Urban Art Worldwide</title>
 </head>
@@ -19,13 +19,6 @@ include_once "header.php";
     <main class="main">
         <img src="./img/graphics/bubbles.png" class="hero-graphic0" alt="hero-graphic0">
 
-        <!-- <div class="onboarding-page2">
-            <h1 class="roboto-uppercase-heading">Urban Art Worldwide</h1>
-            <p class="welcome-heading-small">And new places on the map.</p>
-            <p class="welcome-text">Find your favourite walls and share your work while
-                exploring and travelling the world.</p>
-            <a href="welcome3.php" class="btn">Next</a>
-        </div> -->
         <div class="onboarding-page3">
             <div class="contact-form-wrapper">
 
