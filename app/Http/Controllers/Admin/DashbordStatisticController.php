d4554bb29123c69334bcaebc9cc08d1aec6e3683
Snr Andy <nfonandrew73@gmail.com>
2024-07-17 14:28:13 +0100

    dashboard statistics

--- app/Http/Controllers/Admin/DashbordStatisticController.php
+++ app/Http/Controllers/Admin/DashbordStatisticController.php
@@ -19,7 +19,7 @@ class DashbordStatisticController extends ApiBaseController
         $userCount = User::count();
         $wallsCount = Wall::count();
         $recentArtworks = Artwork::with('user')->get()->take(10);
-        $users = User::with('profile', 'roles')->get();
+        $users = User::with('profile')->get();
 
         $data = compact('artworkCount', 'postCount', 'userCount', 'wallsCount', 'recentArtworks', 'users');
 
