ef7e844dc6d97a95ff7c4407f428774d40c55689
Snr Andy <nfonandrew73@gmail.com>
2024-10-20 18:37:21 +0100

    initiate notifications

---
+++ app/Enums/ActivityType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum ActivityType: string
+{
+    case POST_LIKED = 'post_liked';
+    case POST_COMMENTED = 'post_commented';
+    case ARTWORK_LIKED = 'artwork_liked';
+    case ARTWORK_COMMENTED = 'artwork_commented';
+    case POST_SHARED = 'post_shared';
+}
---
+++ app/Notifications/ActivityNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Enums\ActivityType;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class ActivityNotification extends Notification
+{
+    use Queueable;
+
+    public ActivityType $activityType;
+    public $user;
+    public $entity;
+
+    public function __construct(ActivityType $activityType, $user, $entity)
+    {
+        $this->activityType = $activityType;
+        $this->user = $user;
+        $this->entity = $entity;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database', 'broadcast'];
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'activity_type' => $this->activityType->value, // Using the Enum value here
+            'message' => $this->generateMessage(),
+            'entity_id' => $this->entity->id,
+            'user_id' => $this->user->id,
+        ];
+    }
+
+    private function generateMessage()
+    {
+        return match ($this->activityType) {
+            ActivityType::POST_LIKED => "{$this->user->name} liked your post.",
+            ActivityType::POST_COMMENTED => "{$this->user->name} commented on your post.",
+            ActivityType::ARTWORK_LIKED => "{$this->user->name} liked your artwork.",
+            ActivityType::ARTWORK_COMMENTED => "{$this->user->name} commented on your artwork.",
+            default => "{$this->user->name} performed an action.",
+        };
+    }
+}
