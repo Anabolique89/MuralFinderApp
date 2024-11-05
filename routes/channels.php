7e61179f8fe23d68800cae70843c301e1aace670
Snr Andy <nfonandrew73@gmail.com>
2024-11-05 23:11:31 +0100

    fix: install pusher server

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::private('user.{id}', function ($user, $id) {
+Broadcast::channel('user.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
