3eb80e2e19bc4f2222700a037123b6f945e336af
Snr Andy <nfonandrew73@gmail.com>
2024-11-20 09:44:02 +0100

    reports

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'reportable_id',
+        'reportable_type',
+        'reason',
+    ];
+
+    public function reportable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
